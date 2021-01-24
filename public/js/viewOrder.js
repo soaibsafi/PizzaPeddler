50503fd2367919774f8df5784281e8816b00bd91
Saddaf Afrin Khan <saddaf88@gmail.com>
2021-01-24 20:43:55 +0100

    Fix (View) - Order details

--- public/js/viewOrder.js
+++ public/js/viewOrder.js
@@ -15,11 +15,19 @@ function viewOrderDropDown() {
     var orderDDL = document.createElement("select");
     orderDDL.name = "order";
     orderDDL.id = "orderid";
+
     fetch("http://localhost:3000/getAllOrder").then((response) => {
         response.json().then((data) => {
             console.log(data.orderData);
+           var  opt = new Option(
+                "Select An order",
+                ""
+            );
+
+            orderDDL.append(opt);
+
             for (let i = 0; i < data.orderData.length; i++) {
-                let opt = new Option(
+                 opt = new Option(
                     data.orderData[i].oid,
                     data.orderData[i].oid
                 );
@@ -37,25 +45,45 @@ viewOrderBTN.addEventListener("click", () => {
 })
 
 function viewOrderTable(oid) {
-    console.log(oid)
+
+    var table = document.getElementById("OrderDetailsTBL");
+    var customerNameLBL = document.getElementById("customerNameLBL");
+    var pizzaNameLBL = document.getElementById("pizzaNameLBL");
+
+    console.log(table);
+    if(table) {
+        table.parentNode.removeChild(table);
+        customerNameLBL.parentNode.removeChild(customerNameLBL);
+        pizzaNameLBL.parentNode.removeChild(pizzaNameLBL);
+
+    }
+    if(oid)
     fetch("http://localhost:3000/getAllOrderDetails?oid=" + oid).then((orderDetailsResponse) => {
         orderDetailsResponse.json().then((data) => {
-            //order_ID.setAttribute("id", data.orderDetailsData[0].oid);
-            //var uniqueid = order_ID.id + "_" + pizzaID + "_" + custID + "_" + ingreID
+
+            var customerNameLBL = document.createElement("label");
+            customerNameLBL.setAttribute("id","customerNameLBL");
+            customerNameLBL.innerText = "This order is for : "+data.orderDetailsData[0].cname;
+            document.querySelector("#divCustomarLBL").appendChild(customerNameLBL);
+
+            var pizzaNameLBL = document.createElement("label");
+            pizzaNameLBL.setAttribute("id","pizzaNameLBL")
+            pizzaNameLBL.innerText = "Pizza size is : "+data.orderDetailsData[0].pname;
+            document.querySelector("#divPizzaNameLBL").appendChild(pizzaNameLBL);
 
 
             var body = orderTBL;
             var tbl = document.createElement("table");
+            tbl.setAttribute("id","OrderDetailsTBL")
             var tblBody = document.createElement("tbody");
-            tbl.setAttribute("id", "orderDetailsTable");
+            var ingredientsDataLength = data.orderDetailsData.length;
 
-
-
-            ////Creating cart table
-            for (var i = 0; i < data.orderDetailsData.length; i++){
+            for (var i = 0; i < ingredientsDataLength; i++) {
                 var row = document.createElement("tr");
+
                 for (var j = 0; j < 3; j++) {
                     var cell = document.createElement("td");
+
                     var cellText;
                     switch (j) {
                         case 0:
@@ -65,23 +93,27 @@ function viewOrderTable(oid) {
                             cellText = document.createTextNode(data.orderDetailsData[i].oqty);
                             break;
                         case 2:
-                            cellText = document.createTextNode(data.orderDetailsData[i].sqty);
+                            var status = "In Stock";
+                            if(parseInt(data.orderDetailsData[i].oqty) > parseInt(data.orderDetailsData[i].sqty))
+                                status="Out of stock"
+                            cellText = document.createTextNode(status);
                             break;
                     }
-
                     cell.appendChild(cellText);
                     row.appendChild(cell);
                 }
-            }
 
-            tblBody.appendChild(row);
+                tblBody.appendChild(row);
+            }
 
             tbl.appendChild(tblBody);
             body.appendChild(tbl);
-
             tbl.setAttribute("border", "1");
+
+
+
         })
     })
-
+    else alert ("Please select an order")
 
 }
