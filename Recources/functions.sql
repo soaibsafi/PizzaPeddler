bd6e50874f590fee7eedf319786fe6fe2dbe479e
soaibsafi <soaib.safi@gmail.com>
2021-01-24 23:08:24 +0100

    :sparkles:Add - Order details function

--- Recources/functions.sql
+++ Recources/functions.sql
@@ -765,4 +765,38 @@ from delete_supplier(5);
 
 
 
+-- GEt the details of an order
+CREATE OR REPLACE FUNCTION get_an_order_details(id varchar)
+    RETURNS table
+            (
+                oid VARCHAR,
+                oqty INTEGER,
+                iname text,
+                sQty integer,
+                pname text,
+                cname text,
+                iid integer
 
+--   ,status text
+            ) AS
+$$
+
+BEGIN
+
+    return query
+        select o.o_id, o.quantity as oQty , CONCAT(i.name, ' (', i.regional_provinance, ')')::text, i.quantity as sQty,
+               p.size::text,c.name::text,o.i_id
+        from "order" as o
+
+                 join customer as c on o.c_id = c.c_id
+                 join ingredients as i on o.i_id = i.i_id
+                 join pizza as p on o.p_id = p.p_id
+
+        where
+                o_id=id
+          and o.i_id in (select i_id from ingredients)
+          and o.c_id = c.c_id
+          and o.p_id = p.p_id;
+
+END;
+$$ LANGUAGE plpgsql;
--- Recources/temp.sql
+++ Recources/temp.sql
@@ -1,41 +1,34 @@
-CREATE
-    OR REPLACE FUNCTION update_stock_on_order_served()
-    RETURNS trigger AS
+CREATE OR REPLACE FUNCTION get_an_order_details(id varchar)
+RETURNS table
+ (
+   oid VARCHAR,
+   oqty INTEGER,
+   iname text,
+   sQty integer,
+   pname text,
+   cname text,
+   iid integer
+
+--   ,status text
+) AS
 $$
-BEGIN
-    UPDATE ingredients
-    SET quantity = (SELECT ingredients.quantity
-                    FROM ingredients,
-                         "order"
-                    WHERE ingredients.i_id = OLD.i_id
-                      AND "order".o_id = OLD.o_id
-                   )::INTEGER - OLD.quantity;
-    RETURN OLD;
-
-END;
-$$
-    LANGUAGE 'plpgsql';
-
-
--- Trigger to call removing item from cart after inserting into order
-    CREATE TRIGGER order_served_trigger
-    BEFORE DELETE
-    ON "order"
-    FOR EACH ROW
-EXECUTE PROCEDURE update_stock_on_order_served();
-
-
--- select * from  delete_ingredients(2)
-
-
-CREATE OR REPLACE FUNCTION delete_ingredients(iid INTEGER, name VARCHAR,)
-    RETURNS varchar AS
-$$
-DECLARE
-    res INTEGER;
 
 BEGIN
 
+return query
+select o.o_id, o.quantity as oQty , CONCAT(i.name, ' (', i.regional_provinance, ')')::text, i.quantity as sQty,
+       p.size::text,c.name::text,o.i_id
+from "order" as o
+
+         join customer as c on o.c_id = c.c_id
+         join ingredients as i on o.i_id = i.i_id
+         join pizza as p on o.p_id = p.p_id
+
+where
+        o_id=id
+  and o.i_id in (select i_id from ingredients)
+  and o.c_id = c.c_id
+  and o.p_id = p.p_id;
 
 END;
 $$ LANGUAGE plpgsql;
